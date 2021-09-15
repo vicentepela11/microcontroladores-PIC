fa2759428045d9176ea850a5209a532b1e89f567
Vicente Moita <60237361+vicentepela11@users.noreply.github.com>
2021-09-15 19:42:14 -0300

    Update projeto4.c

--- projeto4.c
+++ projeto4.c
@@ -1,15 +1,43 @@
 
-void main() { 
-   TRISA = 0b00000001;
-   ANSEL = 0b0;
+
+void main() {
+   TRISA = 0b00111001;
+   TRISB = 0b00000000;
+   ANSEL = 0;
+   
    RA1_bit = 0;
+   
+   RB1_bit = 0;
+   RB2_bit = 0;
+   RB3_bit = 0;
    
    while(1){
       if(RA0_bit){
-         delay_ms(20);
+         delay_ms(15);
          if(RA0_bit){
             RA1_bit = ~RA1_bit;
          }
       }while(RA0_bit);
+      
+      if(RA3_bit){
+         delay_ms(15);
+         if(RA3_bit){
+            RB1_bit = ~RB1_bit;
+         }while(RA3_bit);
+      }
+      
+      if(RA4_bit){
+         delay_ms(15);
+         if(RA4_bit){
+            RB2_bit = ~RB2_bit;
+         }
+      }while(RA4_bit);
+      
+      if(RA5_bit){
+         delay_ms(15);
+         if(RA5_bit){
+            RB3_bit = ~RB3_bit;
+         }
+      }while(RA5_bit);
    }
 }
